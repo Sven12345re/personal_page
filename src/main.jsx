a0bf65e53e39862b80a03bd700359d5272f3f694
Sven12345re <sven.redlich96@gmail.com>
2025-12-19 18:59:28 +0100

    change title again

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import Card from "./components/Card.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Card
-      title="My title test 2"
+      title="My title test 3"
       description="My description"
       image="https://picsum.photos/200/300"
     />
